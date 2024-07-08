884a69452afcb1c532f7cedafcd640f5bfa58d0e
LUCAS TELES DOS SANTOS <LUCAS6263@DIGITALCOLLEGE.LOCAL>
2024-07-08 16:43:19 -0300

    Atividade

---
+++ script.js
@@ -0,0 +1,6 @@
+const opcoes = {
+    1 : {nome: 'pedra', img: 'img/pedra.png', pedra: 'EMPATE', papel: 'PERDEU', tesoura: 'GANHOU'},
+    2 : {nome: 'papel', img: 'img/papel.png', pedra: 'GANHOU', papel: 'EMPATE', tesoura: 'GANHOU'},
+    3 : {nome: 'tesoura', img: 'img/tesoura.png', pedra: 'PERDEU', papel: 'GANHOU', tesoura: 'EMPATE'}
+}
+
